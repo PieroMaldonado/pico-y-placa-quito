a84df356cb31a4e74d7d7e1b9b046757fcd2711c
unknown <pieromaldonado16@gmail.com>
2024-08-08 08:51:48 -0500

    crear controlador PicoPlacaController

---
+++ Controllers/PicoPlacaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PicoYPlacaQuito.Models;
+
+namespace PicoYPlacaQuito.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PicoPlacaController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public PicoPlacaController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegistrarConsulta([FromBody] ConsultaRequest request)
+        {
+            return Ok("Mensaje");
+        }
+    }
+}
