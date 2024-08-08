67dd3bb55b993b5b42a5d259e3e34e8475266605
unknown <pieromaldonado16@gmail.com>
2024-08-08 08:39:48 -0500

    agregar modelo de datos

---
+++ Models/ConsultaRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace PicoYPlacaQuito.Models
+{
+    public class ConsultaRequest
+    {
+        public string Placa { get; set; }
+        public DateTime Fecha { get; set; }
+        public TimeSpan Hora { get; set; }
+    }
+}
---
+++ Models/ConsultaResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace PicoYPlacaQuito.Models
+{
+    public class ConsultaResponse
+    {
+        public bool PuedeCircular { get; set; }
+        public string Mensaje { get; set; }
+    }
+}
